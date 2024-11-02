0f90493a25ac3bd4a3ea8d29c030f3dea8927ddc
Kazuhiro Ito <kazuhiroito0127@gmail.com>
2024-11-02 13:58:47 +0000

    EvenOddChecker.hsを追加

---
+++ app/EvenOddChecker.hs
@@ -0,0 +1,9 @@
+module Main where
+
+main ::IO ()
+main = do
+  putStrLn "Enter a numbr:"
+  num <- readLn ::IO Int
+  if even num
+    then putStrLn $ show num ++ " is even."
+    else putStrLn $ show num ++ " is odd."
