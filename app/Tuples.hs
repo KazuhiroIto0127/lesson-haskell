81a14a6166b5cf6b167f96bb59051076967d3e97
Kazuhiro Ito <kazuhiroito0127@gmail.com>
2024-11-04 13:18:05 +0900

    Create Tuples.hs

---
+++ app/Tuples.hs
@@ -0,0 +1,17 @@
+module Main where
+
+main ::IO ()
+main = do
+  let xs = [1,2,3,4] :: [Integer]
+  let ys = [5,6,7,9] :: [Integer]
+  let cs = ['a','b','c'] :: [Char]
+  putStrLn $ "x: " ++ show xs
+  putStrLn $ "y: " ++ show ys
+  putStrLn $ "c: " ++ cs
+
+  putStrLn "------- zip --------------"
+  putStrLn $ "zip x and y: " ++ show(zip xs ys)
+  putStrLn $ "zip x and c: " ++ show(zip xs cs)
+
+  putStrLn "------- manipulate tuples -----------"
+  putStrLn $ "fst (1,5): " ++ show(fst (1 :: Integer, 5 :: Integer))
