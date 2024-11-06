12c31cd21ffa6782683edee2be6748214791e49c
Kazuhiro Ito <kazuhiroito0127@gmail.com>
2024-11-06 14:02:11 +0000

    sayMe関数の追加

--- app/Function.hs
+++ app/Function.hs
@@ -13,9 +13,18 @@ sum' :: Num n => [n] -> n
 sum' [] = 0
 sum' (x:xs) = x + sum' xs
 
+sayMe :: (Integral a) => a -> String
+sayMe 1 = "One!"
+sayMe 2 = "Two!"
+sayMe 3 = "Three!"
+sayMe 4 = "Four!"
+sayMe 5 = "Five!"
+sayMe _ = "Not between 1 and 5"
+
 main ::IO ()
 main = do
   putStrLn $ removeNonUppercase "A Ha Ho hi"
   putStrLn $ show (addThree 1 2 3)
   putStrLn $ show (factorial 50)
   putStrLn $ show (sum' [1,2,3,5] :: Integer)
+  putStrLn $ sayMe (1 :: Integer)
