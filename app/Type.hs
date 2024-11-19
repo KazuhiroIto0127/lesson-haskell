ee6d41b4861550f09ed7189b8420a5dae271c9dd
Kazuhiro Ito <kazuhiroito0127@gmail.com>
2024-11-19 22:17:00 +0900

    Type.hsにコード追加

--- app/Type.hs
+++ app/Type.hs
@@ -5,6 +5,18 @@ newtype C = C Double deriving Show
 
 convert :: C -> K
 convert (C x) = K (x + 273.15)
+
+data HTTPStatus = OK
+                | Found
+                | NotFound
+                | ServiceUnavailable
+                deriving (Show)
+
+statusMessage :: HTTPStatus -> String
+statusMessage OK                 = "Everything is fine."
+statusMessage Found              = "The resource has been found."
+statusMessage NotFound           = "The resource could not be found."
+statusMessage ServiceUnavailable = "The service is unavailable right now."
     
 main :: IO ()
 main = do
@@ -13,4 +25,5 @@ main = do
     putStrLn $ show tempC
     putStrLn $ show tempK
     putStrLn $ show (convert tempC)
+    putStrLn $ "Status: " ++ show OK ++ " -> " ++ statusMessage OK
     
